466019939515527cd3349e0c5ef30c331eec1bbb
liyh <liyh@zg900.com>
2019-09-02 17:00:37 +0800

    s

--- src/components/video.vue
+++ src/components/video.vue
@@ -14,17 +14,23 @@ export default {
             config: {
                 id: 'vs',
                 // fluid: true,
-                width: 'auto',
+                // width: 'auto',
                 height: '213px',
-                pip: true,
+                volume: 0.3,
+                width: window.innerWidth,
+                // pip: true,   // 画中画
                 lang: 'zh-cn',
-                videoInit: true,
-                download: true, //设置download控件显示
+                videoInit: true, // 是否显示初始帧数
+                // download: true, //设置download控件显示
                 // playbackRate: [0.7, 1.0, 1.5, 2.0], //播放速度
                 autoplay: false, //如果true,浏览器准备好时开始回放。
                 url: require('../../static/video/video_1.mp4'),
                 // poster: require("../../static/video/video_1_gif.gif"), //你的封面地址
-                'x5-video-player-fullscreen': false, // 微信的全屏播放
+                // 'x5-video-player-fullscreen': false, // 微信的全屏播放
+                "playsinline": true,
+                "whitelist": [
+                    ""
+                ]
             },
             Player: null
         };
