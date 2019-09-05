48cbf4878a139fe2a61434f22f9b105d65680715
ITLYH <mail.786835033@qq.com>
2019-09-06 01:27:28 +0800

    sj

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,50 @@
 <template>
-    <div id="c">
-        <router-view>
-            <login-view></login-view>
-        </router-view>
-        
-    </div>
+  <div id="c">
+    <router-view>
+      <login-view></login-view>
+    </router-view>
+  </div>
 </template>
 
 <script>
 import login from "./view/login";
 export default {
-    data(){
-        return {
-
+  data() {
+    return {};
+  },
+  components: {
+    "login-view": login
+  },
+  watch: {
+    $route(to, from) {
+      // if (to.path === '/main' || to.path === '/lifeInfo') {
+      //     this.$store.commit("isFlag", false);
+      // } else {
+      //     this.$store.commit("isFlag", true);
+      // }
+      const that = this;
+      if (to.path === "/login") {
+        var token = localStorage.getItem("token");
+        if (!token) {
+          that.$router.push({ path: "/main" });
         }
-    },
-    components:{
-        "login-view" : login,
-    },
-    watch: {
-        $route(to, from) {
-            // if (to.path === '/main' || to.path === '/lifeInfo') {
-            //     this.$store.commit("isFlag", false);
-            // } else {
-            //     this.$store.commit("isFlag", true);
-            // }
-        }
-    },
+      }
+    }
+  }
 };
 </script>
 
 <style>
-@import '../static/css/common.css';
-@import '../static/font/iconfont.css';
+@import "../static/css/common.css";
+@import "../static/font/iconfont.css";
 #c {
-    font-family: "微软雅黑","Microsoft YaHei","Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    padding: 0;
-    overflow: hidden;
+  font-family: "微软雅黑", "Microsoft YaHei", "Avenir", Helvetica, Arial,
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding: 0;
+  overflow: hidden;
 }
 </style>
