3e7009c648f5ae9b4a4308c527ebd443e1ef13a5
liyh <liyh@zg900.com>
2019-11-26 16:47:09 +0800

    2019年11月26日 16:47:04

--- src/main.js
+++ src/main.js
@@ -8,23 +8,28 @@ import axios from 'axios'
 import Vant from 'vant';
 import { Lazyload } from 'vant';
 import 'vant/lib/index.css';
+import less from 'less'
 
 // import './config/rem'
 
 Vue.config.productionTip = false
+
 Vue.use(Vant);
 Vue.use(Lazyload);
+
+Vue.use(less)
+// 在组件内使用 <style lang="less" scoped></style>
 
 Vue.use(VueRouter)
 Vue.use(Vuex)
 
 Vue.use(axios)
-// Vue.prototype.$axios = axios
+// Vue.prototype.$axios = axios   // 挂载到vue
 axios.defaults.baseURL = 'http://localhost:8080/';
 axios.defaults.headers.common['token'] = localStorage.getItem('token');
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-
+// 全局路由监控
 router.beforeEach((to, from, next) => {
   if (to.meta.permission) {
     var token = localStorage.getItem('token');
